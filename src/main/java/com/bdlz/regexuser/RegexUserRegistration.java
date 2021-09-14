ac5bbedc272ef088029d6568a8bc7aa2244262a4
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-14 21:36:44 +0530

    Added: User First Name

--- src/main/java/com/bdlz/regexuser/RegexUserRegistration.java
+++ src/main/java/com/bdlz/regexuser/RegexUserRegistration.java
@@ -1,7 +1,31 @@
 package com.bdlz.regexuser;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Author: Ramakrishna
+ * Program: RegexUserRegistration
+ * Since: 14/9/21
+ */
 public class RegexUserRegistration {
     public static void main(String[] args) {
-        System.out.println("Welcome to Regex User Registration Problem");
+
+        System.out.println("Enter Your First Name");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        String regex = "^[A-Z]{1}[a-z]{3,}$";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.matches()) {
+            System.out.println("Valid");
+        }else {
+            System.out.println("Invalid");
+        }
     }
 }
